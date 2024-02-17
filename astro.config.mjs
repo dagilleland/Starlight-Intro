564206587a8fd8cef3c0f13fb31063005e44887d
Dan Gilleland <dagilleland@shaw.ca>
2024-02-17 09:18:47 -0700

    Try another fix for site in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://gilleland.ca',
-	base: '/starlight-intro',	
+	base: '/Starlight-Intro',	
 	integrations: [
 		starlight({
 			title: 'My Docs',
